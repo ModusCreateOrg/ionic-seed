8e4d79169f0d3dff1e690ed1c492869ae8172006
Carlos Paelinck <carlos@carlospaelinck.com>
2015-12-28 16:45:36 -0600

    Refactored controllers for starter about module.

---
+++ src/app/components/about/about.controllers.js
@@ -0,0 +1,5 @@
+import AboutController from './about.controller';
+
+export default angular
+    .module('ionicSeed.about.controllers', [])
+    .controller('AboutController', AboutController);
--- src/app/components/about/about.module.js
+++ src/app/components/about/about.module.js
@@ -1,9 +1,12 @@
 import aboutRun from './about.run';
 import aboutConfig from './about.config';
+import aboutControllers from './about.controllers';
 
 const aboutModule = angular
-  .module('ionicSeed.about', [])
-  .run(aboutRun)
-  .config(aboutConfig)
+    .module('ionicSeed.about', [
+        'ionicSeed.about.controllers'
+    ])
+    .run(aboutRun)
+    .config(aboutConfig)
 
 export default aboutModule;
